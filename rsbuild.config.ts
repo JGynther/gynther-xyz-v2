7a11692d1f5a18df4628184aac22470d9b6c323a
Joona Gynther <joona@gynther.xyz>
2024-04-20 20:23:47 +0300

    attempt to add million

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import million from "million/compiler";
 
 export default defineConfig({
   plugins: [pluginReact()],
+  tools: {
+    rspack: {
+      plugins: [million.rspack({ auto: true })],
+    },
+  },
 });
