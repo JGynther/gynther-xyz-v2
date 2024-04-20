a31948f1f858c0a4edf0b308297892831a717717
Joona Gynther <joona@gynther.xyz>
2024-04-20 22:49:30 +0300

    add banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import "./App.css";
+import Banner from "@components/banner";
 
 const App = () => {
   return (
     <div className="p-5">
+      <Banner />
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
