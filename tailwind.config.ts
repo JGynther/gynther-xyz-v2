ecf78961f80a3d5b4f863963644164dda33c0fcd
Joona Gynther <joona@gynther.xyz>
2024-06-27 02:53:22 +0300

    remove quotes from blockquote in prose

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,16 @@ import Typography from "@tailwindcss/typography";
 export default {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            "blockquote p:first-of-type::before": null,
+            "blockquote p:first-of-type::after": null,
+          },
+        },
+      },
+    },
   },
   plugins: [Typography],
 } satisfies Config;
